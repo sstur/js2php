fee6ce94e9ba69dcc73c9f93b5d7e667ed8b1fa0
Simon Sturmer <sstur@me.com>
2014-09-15 17:59:11 -0700

    update docs for Ex

--- php/classes/Exception.php
+++ php/classes/Exception.php
@@ -13,26 +13,19 @@ class Ex extends Exception {
     $this->value = $value;
   }
 
-  //since we use a custom exception handler we need to render our own stack trace
+  /**
+   * Since we use a custom exception handler we need to render our own stack trace
+   * @param Exception $ex
+   */
   static function handleException($ex) {
     echo $ex->getFile() . ':' . $ex->getLine() . "\n";
     echo $ex->getMessage() . "\n";
     $stack = $ex->getTrace();
+    //the first frame is not on the stack, so add it
     array_unshift($stack, array('line' => $ex->getLine(), 'file' => $ex->getFile()));
     foreach ($stack as $frame) {
       echo '    at ' . $frame['file'] . ':' . $frame['line'] . "\n";
     }
-    //$trace = 'Fatal error: Uncaught ' . $ex->__toString();
-    //Debug::log("__toString:\n" . $trace . "\n----------");
-    //echo 'Fatal error: Uncaught exception: ' . $ex->getMessage() . ' in ' . $ex.getFile() . ':' . $ex.getLine() . "\n";
-    //$className = get_class($ex);
-    //$message = $ex->getMessage();
-    //$err = array(
-    //  "Fatal error: Uncaught exception: " . $message,
-    //  "Stack trace:",
-    //  $ex->getTraceAsString()
-    //);
-    //echo join("\n", $err) . "\n";
   }
 
 }
