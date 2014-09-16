ca14cb5c413cbe0bb0f5878b343527416a949507
Simon Sturmer <sstur@me.com>
2014-09-15 17:05:10 -0700

    add global exception handler

--- php/classes/Exception.php
+++ php/classes/Exception.php
@@ -3,17 +3,38 @@ class Ex extends Exception {
   public $value = null;
 
   function __construct($value) {
+    if ($value instanceof Error) {
+      $message = $value->get('message');
+      $message = $message ? 'Error: ' . $message : 'Error';
+    } else {
+      $message = to_string($value);
+    }
+    parent::__construct($message);
     $this->value = $value;
   }
 
-  //todo: if we use __toString we need to render our own stack trace
-
-  //function __toString() {
-  //  if ($this->value instanceof Error) {
-  //    return $this->value->get('message');
-  //  } else {
-  //    return to_string($this->value);
-  //  }
-  //}
+  //since we use a custom exception handler we need to render our own stack trace
+  static function handleException($ex) {
+    echo $ex->getFile() . ':' . $ex->getLine() . "\n";
+    echo $ex->getMessage() . "\n";
+    $stack = $ex->getTrace();
+    array_unshift($stack, array('line' => $ex->getLine(), 'file' => $ex->getFile()));
+    foreach ($stack as $frame) {
+      echo '    at ' . $frame['file'] . ':' . $frame['line'] . "\n";
+    }
+    //$trace = 'Fatal error: Uncaught ' . $ex->__toString();
+    //Debug::log("__toString:\n" . $trace . "\n----------");
+    //echo 'Fatal error: Uncaught exception: ' . $ex->getMessage() . ' in ' . $ex.getFile() . ':' . $ex.getLine() . "\n";
+    //$className = get_class($ex);
+    //$message = $ex->getMessage();
+    //$err = array(
+    //  "Fatal error: Uncaught exception: " . $message,
+    //  "Stack trace:",
+    //  $ex->getTraceAsString()
+    //);
+    //echo join("\n", $err) . "\n";
+  }
 
 }
+
+set_exception_handler(array('Ex', 'handleException'));
