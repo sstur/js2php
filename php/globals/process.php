9ff21ac5d7636ea6fa70ef2368e05ff648b2f3ee
Simon Sturmer <sstur@me.com>
2014-09-20 15:42:32 -0700

    refactor http req/res to exist on process

--- php/globals/process.php
+++ php/globals/process.php
@@ -1,5 +1,9 @@
 <?php
 $process = new Object();
+
+// the type of interface between web server and PHP
+$process->set('sapi_name', php_sapi_name());
+
 $process->set('exit', new Func(function($this_, $arguments, $code = 0) {
   $code = intval($code);
   exit($code);
--- php/http/Request.php
+++ php/http/Request.php
@@ -1,71 +1,34 @@
 <?php
-class Request extends Object {
-
-  static $protoObject = null;
-  static $classMethods = null;
-  static $protoMethods = null;
-
-  function __construct() {
-    parent::__construct();
-    $this->proto = self::$protoObject;
-    if (func_num_args() > 0) {
-      $this->init(func_get_args());
-    }
-  }
-
-  function init($args) {
-    //$this->set('_super', $args[0]);
-  }
-
-  static function initProtoMethods() {
-    $protoObject = new Object();
-    $protoObject->setMethods(Request::$protoMethods, true, false, true);
-    Request::$protoObject = $protoObject;
-  }
-
-}
-
-Request::$classMethods = array();
-
-Request::$protoMethods = array(
-  'getMethod' => function() {
-      return $_SERVER['REQUEST_METHOD'];
-    },
-  'getURL' => function() {
-      //return $_SERVER['REQUEST_URI'] . $_SERVER['QUERY_STRING'];
-      return $_SERVER['REQUEST_URI'];
-    },
-  'getHeaders' => function() {
-      $headers = new Object();
-      foreach ($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) === 'HTTP_') {
-          $key = strtolower(substr($key, 5));
-          $key = str_replace('_', '-', $key);
-          $headers->set($key, $value);
+$process->set('request', call_user_func(function() {
+  $headers = null;
+  $methods = array(
+    'getMethod' => function() {
+        return $_SERVER['REQUEST_METHOD'];
+      },
+    'getURL' => function() {
+        return $_SERVER['REQUEST_URI'];
+      },
+    'getHeaders' => function() use (&$headers) {
+        if ($headers === null) {
+          $headers = new Object();
+          foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) === 'HTTP_') {
+              $key = strtolower(substr($key, 5));
+              $key = str_replace('_', '-', $key);
+              $headers->set($key, $value);
+            }
+          }
         }
+        return $headers;
+      },
+    'getRemoteAddress' => function() {
+        return $_SERVER['SERVER_ADDR'];
+      },
+    'read' => function($bytes) {
+        throw new Ex(Error::create('not implemented: Request.read()'));
       }
-      return $headers;
-    },
-  'getRemoteAddress' => function() {
-      return $_SERVER['SERVER_ADDR'];
-    },
-  'read' => function($bytes) {
-      throw new Ex(Error::create('not implemented: Request.read()'));
-    }
-);
-
-Request::initProtoMethods();
-
-if (!isset($PHP)) {
-  $PHP = new Object();
-}
-$PHP->set('Request', call_user_func(function() {
-  $Request = new Func('Request', function($this_, $arguments) {
-    $self = new Request();
-    $self->init($arguments->args);
-    return $self;
-  });
-  $Request->setMethods(Request::$classMethods, true, false, true);
-  $Request->set('sapi_name', php_sapi_name());
-  return $Request;
+  );
+  $request = new Object();
+  $request->setMethods($methods, true, false, true);
+  return $request;
 }));
