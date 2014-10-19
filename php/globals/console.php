93d93f788437c1de4a2f32e1913c0ce5a5585719
Simon Sturmer <sstur@me.com>
2014-10-18 23:33:11 -0700

    fix console.log truncating output

--- php/globals/console.php
+++ php/globals/console.php
@@ -24,22 +24,31 @@ $console = call_user_func(function() {
     return join(' ', $output) . "\n";
   };
 
+  //some platforms require multiple calls to fwrite
+  $writeAll = function($stream, $data) {
+    $bytesTotal = strlen($data);
+    $bytesWritten = 0;
+    while ($bytesWritten < $bytesTotal) {
+      $bytesWritten += fwrite($stream, substr($data, $bytesWritten));
+    }
+  };
+
   $console = new Object();
 
-  $console->set('log', new Func(function($this_, $arguments) use (&$stdout, &$toString) {
+  $console->set('log', new Func(function($this_, $arguments) use (&$stdout, &$toString, &$writeAll) {
     if ($stdout === null) {
       $stdout = fopen('php://stdout', 'w');
     }
     $output = $toString($arguments->args);
-    fwrite($stdout, $output);
+    $writeAll($stdout, $output);
   }));
 
-  $console->set('error', new Func(function($this_, $arguments) use (&$stderr, &$toString) {
+  $console->set('error', new Func(function($this_, $arguments) use (&$stderr, &$toString, &$writeAll) {
     if ($stderr === null) {
       $stderr = fopen('php://stderr', 'w');
     }
     $output = $toString($arguments->args);
-    fwrite($stderr, $output);
+    $writeAll($stderr, $output);
   }));
 
   return $console;
