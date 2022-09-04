e6b3d9048ca335ad8aed9dedce6e78e5cd42dfa7
Immanuel Scheerer <mail@immanuelscheerer.de>
2022-09-04 16:15:46 +0200

    Introduced basic infrstastructure for performance tests: via "npm run performance-js" the performance of running tests with is evaluated. Every tests is run 1000 times. Same is the case for php with "npm run performance-php".

--- performance-tests.js
+++ performance-tests.js
@@ -1,20 +1,43 @@
 #!/usr/bin/env node
 var processStartInMs = Date.now();
-var tests = ['core', 'number', 'boolean', 'string', 'date', 'regex', 'array', 'buffer', 'json']; //, 'module-fs'];
+var tests = [
+  'core',
+  'number',
+  'boolean',
+  'string',
+  'date',
+  'regex',
+  'array',
+  'buffer',
+  'json',
+];
 require('./test/helpers');
-for(var i in tests) {
-    var test = tests[i];
-    var path = './test/' + test;
-    var startTimeInMs = Date.now();
-    var numOfLoops = 1000;
-    for (var j = 0; j < numOfLoops; j++) {
-        delete require.cache[require.resolve(path)];
-        require(path);
-    }
-    var endTimeInMs = Date.now();
-    var duration = endTimeInMs - startTimeInMs;
-    console.log(test + ', total: ' + duration + ' ms, each run: ' + (duration / numOfLoops) + ' ms');
+for (var i in tests) {
+  var test = tests[i];
+  var path = './test/' + test;
+  var startTimeInMs = Date.now();
+  var numOfLoops = 1000;
+  for (var j = 0; j < numOfLoops; j++) {
+    delete require.cache[require.resolve(path)];
+    require(path);
+  }
+  var endTimeInMs = Date.now();
+  var duration = endTimeInMs - startTimeInMs;
+  console.log(
+    test +
+      ', total: ' +
+      duration +
+      ' ms, each run: ' +
+      duration / numOfLoops +
+      ' ms'
+  );
 }
 var processEndInMs = Date.now();
 var processDuration = processEndInMs - processStartInMs;
-console.log('process total: ' + processDuration + ' ms, each run: ' + (processDuration / numOfLoops) + ' ms');
+console.log(
+  'process total: ' +
+    processDuration +
+    ' ms, each run: ' +
+    processDuration / numOfLoops +
+    ' ms'
+);
